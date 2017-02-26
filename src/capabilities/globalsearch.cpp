be078280ee07ff12787d2cd4054d78227b660473
contra98 <Conrad.Huebler@student.tu-freiberg.de>
2017-02-26 02:05:53 +0100

    add some files, might be forgotten

---
+++ src/capabilities/globalsearch.cpp
@@ -0,0 +1,229 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2017  Conrad Hübler <Conrad.Huebler@gmx.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include "src/core/minimizer.h"
+#include "src/core/models.h"
+
+#include <QtCore/QCoreApplication>
+#include <QtCore/QThreadPool>
+#include <QtCore/QVector>
+#include <QtCore/QObject>
+#include <QtCore/QDateTime>
+#include <QtCore/QPointF>
+#include <QtCore/QList>
+
+#include <iostream>
+
+
+#include "globalsearch.h"
+
+GlobalSearch::GlobalSearch(QObject *parent) : QObject(parent), m_minimizer(QSharedPointer<Minimizer>(new Minimizer(this), &QObject::deleteLater))
+{
+    
+    
+}
+
+GlobalSearch::~GlobalSearch()
+{
+    
+    
+}
+void GlobalSearch::Interrupt()
+{
+    m_allow_break = true; 
+    QThreadPool::globalInstance()->clear();
+}
+
+QVector<QVector<double> > GlobalSearch::ParamList() 
+{
+    int max_count = 1;
+    
+    m_time = 0;
+    QVector< QVector<double > > full_list;
+    for(int i = 0; i < m_parameter.size(); ++i)
+    {
+        QVector<double > list;
+        double min = 0, max = 0, step = 0;
+        min = m_parameter[i][0];
+        max = m_parameter[i][1];
+        step = m_parameter[i][2];
+        max_count *= (max+step-min)/step;
+        for(double s = min; s <= max; s += step)
+            list << s;
+        full_list << list;
+    }
+    emit setMaximumSteps(max_count);
+    return full_list;
+}
+
+QList<QJsonObject > GlobalSearch::SearchGlobal()
+{
+    QVector< QVector<double > > full_list = ParamList();
+    m_models_list.clear();
+    QVector<double > error; 
+    m_type |= OptimizationType::ComplexationConstants;
+    int t0 = QDateTime::currentMSecsSinceEpoch();
+    ConvertList(full_list, error);
+    int t1 = QDateTime::currentMSecsSinceEpoch();
+    std::cout << "time for scanning: " << t1-t0 << " msecs." << std::endl;
+    return m_models_list;
+}
+
+QVector<VisualData> GlobalSearch::Create2DPLot()
+{
+    QVector< QVector<double > > full_list = ParamList();
+    m_models_list.clear();
+    QVector<double > error; 
+
+    int t0 = QDateTime::currentMSecsSinceEpoch();
+    ConvertList(full_list, error);
+    int t1 = QDateTime::currentMSecsSinceEpoch();
+    std::cout << "time for scanning: " << t1-t0 << " msecs." << std::endl;
+    return m_3d_data;
+}
+
+
+void GlobalSearch::ConvertList(const QVector<QVector<double> >& full_list, QVector<double > &error)
+{  
+    m_full_list.clear();
+    QVector<int > position(full_list.size(), 0);
+    int maxthreads =qApp->instance()->property("threads").toInt();
+    QVector<QVector<QPointer<NonLinearFitThread> > > threads;
+    QVector<QPointer<NonLinearFitThread> > thread_rows;
+    QThreadPool::globalInstance()->setMaxThreadCount(maxthreads -1 );
+    m_allow_break = false;
+    while(!m_allow_break)
+    {
+        QCoreApplication::processEvents();
+        QList<double > parameter; 
+        for(int j = 0; j < position.size(); ++j)
+            parameter << full_list[j][position[j]];
+        
+        bool temporary = true;
+        for(int i = 0; i < position.size(); ++i)
+        {
+            temporary = temporary && (position[i] == full_list[i].size() - 1);
+        }
+        if(temporary)
+            m_allow_break = true;
+        m_model->setConstants(parameter);
+        m_full_list.append( parameter );
+        
+        QPointer< NonLinearFitThread > thread = m_minimizer.data()->addJob(m_model, m_type);
+        thread_rows << thread;
+        connect(thread, SIGNAL(finished(int)), this, SIGNAL(SingeStepFinished(int)), Qt::DirectConnection);
+        
+        if(m_model->SupportThreads())
+            QThreadPool::globalInstance()->waitForDone();
+        
+        for(int k = position.size() - 1; k >= 0; --k)
+        {
+            if(position[k] == ( full_list[k].size() - 1) )
+            {
+                position[k] = 0;
+                if(position[k] <= full_list[k].size() - 1)
+                {
+                    threads << thread_rows;
+                    thread_rows.clear();
+                }
+                
+            }
+            else
+            {
+                position[k]++;
+                if(position[k] == full_list[k].size())
+                {
+                    position[k] = 0;
+                    if(k > 0)
+                        position[k - 1]++;
+                }
+                break;
+            }
+        }
+    }
+    
+    if(!m_model->SupportThreads())
+    {
+        QThreadPool::globalInstance()->setMaxThreadCount(maxthreads);
+        while(QThreadPool::globalInstance()->activeThreadCount())
+        {
+            QCoreApplication::processEvents();
+        }
+    }
+    
+    for(int i = 0; i < threads.size(); ++i)
+    {
+        VisualData dataRow1;
+        for(int j = 0; j < threads[i].size(); ++j)
+        {
+            QCoreApplication::processEvents();
+            QList< qreal > parameter = threads[i][j]->Model()->Constants();
+            
+            QJsonObject json = threads[i][j]->ConvergedParameter();
+            m_model->ImportJSON(json);
+            m_model->CalculateSignal();
+            double current_error = m_model->ModelError();
+            error << current_error; 
+            if(error_max < current_error)
+                error_max = current_error;
+            last_result.m_error = error;
+            last_result.m_input = full_list;
+            if(m_type & OptimizationType::ComplexationConstants)
+                m_models_list << json;
+            else
+                dataRow1.data.append(QVector<qreal >() << parameter[0] << m_model->SumofSquares() << parameter[1]);
+            delete threads[i][j];
+        }
+        if(!(m_type & OptimizationType::ComplexationConstants))
+            m_3d_data << dataRow1;
+    }
+    
+    return;
+}
+
+QList<QList<QPointF> > GlobalSearch::LocalSearch()
+{
+    QVector< QVector<double > > full_list = ParamList();
+    Scan(full_list);
+    return m_series;
+}
+
+
+void GlobalSearch::Scan(const QVector< QVector<double > >& list)
+{
+    for(int i = 0; i < m_series.size(); ++i)
+        m_series[i].clear();
+    m_series.clear();
+    QVector<double > error;
+    for(int j = 0; j < list.size(); ++j)
+    {
+        QList<QPointF> series;
+        for(int i = 0; i < list[j].size(); ++i)
+        {
+            m_model->setConstants(QList<qreal> () << list[j][i]);
+            m_model->CalculateSignal();
+            error << m_model->ModelError();
+            series.append(QPointF(list[j][i],m_model->ModelError( )));
+        }
+        m_series << series;
+    }
+}
+
+
+#include "globalsearch.moc"
---
+++ src/capabilities/globalsearch.h
@@ -0,0 +1,87 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2017  Conrad Hübler <Conrad.Huebler@gmx.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef GLOBALSEARCH_H
+#define GLOBALSEARCH_H
+
+#include "src/core/models.h"
+
+#include <QtCore/QVector>
+#include <QtCore/QList>
+#include <QtCore/QPointF>
+#include <QtCore/QObject>
+
+class Minimizer;
+class AbstractTitrationModel;
+
+struct GlobalSearchResult
+{
+    QVector< QVector<double > > m_input;  
+    QVector< double > m_error;
+    QVector< double > m_corr_coeff;
+    QVector< QJsonObject > m_models;
+};
+
+struct VisualData
+{
+    QVector<QVector<qreal > > data;
+};
+
+class GlobalSearch : public QObject
+{
+    Q_OBJECT
+    
+public:
+    GlobalSearch(QObject *parent = 0);
+    ~GlobalSearch();
+    inline void setParameter(const QVector<QVector< qreal > > &parameter) { m_parameter = parameter; }
+    inline void setOptimizationFlags(OptimizationType type) { m_type = type; }
+    inline void setModel(const QSharedPointer<AbstractTitrationModel> model) { m_model = model->Clone(); }
+    inline QVector<QList<qreal > > FullList() const { return m_full_list; }
+    QList<QList<QPointF> >  LocalSearch();
+    QList<QJsonObject > SearchGlobal();
+    QVector<VisualData> Create2DPLot();
+    
+public slots:
+    void Interrupt();
+    
+private:
+    QVector<QVector<double> > ParamList();
+    void ConvertList(const QVector<QVector<double> >& full_list, QVector<double > &error);
+    void Scan(const QVector< QVector<double > >& list);
+    QVector<QVector < qreal > > m_parameter;
+    quint64 m_time_0;
+    OptimizationType m_type;
+    int m_time;
+    QList<QJsonObject > m_models_list;
+    QSharedPointer<AbstractTitrationModel> m_model;
+    QList< QList<QPointF> > m_series;
+    QVector<QList<double> > m_full_list;
+    GlobalSearchResult last_result;
+    double error_max;
+    QSharedPointer<Minimizer> m_minimizer;
+    bool m_allow_break;
+    QVector<VisualData> m_3d_data;
+    
+signals:
+    void SingeStepFinished(int time);
+    void setMaximumSteps(int maxsteps);
+};
+
+#endif // GLOBALSEARCH_H
