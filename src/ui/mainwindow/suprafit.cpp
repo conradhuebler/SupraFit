c654d62db487e5c343f168e1be5542ae2fda296d
contra98 <hueblerc@mailserver.tu-freiberg.de>
2017-04-25 14:03:14 +0200

    visible splitter handler

--- src/ui/mainwindow/suprafit.cpp
+++ src/ui/mainwindow/suprafit.cpp
@@ -184,6 +184,15 @@ MainWindow::MainWindow() : m_ask_on_exit(true)
     ReadGeometry();
     LogFile();
     setActionEnabled(false);
+    setStyleSheet("QSplitter::handle:vertical {"
+                    "background: qlineargradient(x1:0, y1:0, x2:1, y2:1,"
+                    "stop:0 #eee, stop:1 #ccc);"
+                    "border: 1px solid #777;"
+                    "height: 1px;"
+                    "margin-top: 2px;"
+                    "margin-bottom: 2px;"
+                    "border-radius: 4px;"
+                    "}");
 }
 
 MainWindow::~MainWindow()
